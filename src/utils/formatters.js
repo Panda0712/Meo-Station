a1a9fe07c622a93213a76aff33367d2dae6d18f3
Pham Tuan <tuan.pham.super@gmail.com>
2025-04-23 00:16:21 +0700

    handle ui logic for booking nights, booking day

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -1,9 +1,23 @@
+import { toZonedTime } from "date-fns-tz";
+
 export function capitalizeWords(str) {
   return str
     ?.split(" ") // Split string into words
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize first letter of each word
     .join(" "); // Join words back into a sentence
 }
+
+export const formatVND = (price) => {
+  return price.toLocaleString("vi-VN");
+};
+
+export const toVNISOString = (date, time) => {
+  const timeZone = "Asia/Ho_Chi_Minh";
+  const fullDateString = `${date}T${time}:00`; // ví dụ: "2025-04-23T08:00:00"
+  const dateObj = new Date(fullDateString);
+  const zonedDate = toZonedTime(dateObj, timeZone);
+  return zonedDate.toISOString();
+};
 
 export const interceptorLoadingElements = (calling) => {
   // DOM lấy ra toàn bộ phần tử trên page hiện tại có className là 'interceptor-loading'
